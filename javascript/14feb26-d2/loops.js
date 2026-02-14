376ad7fdc80eaafd578ea74576b2dbb5f173515d
lumbri7 <lumbrinitin@gmail.com>
2026-02-14 13:21:46 +0530

    loops started in javascript

---
+++ javascript/14feb26-d2/loops.js
@@ -0,0 +1,5 @@
+//loops in javascript
+//a single line of code can fuck somemeone as mush as you want
+
+for(let count = 0; count <= 9999999; count++){console.log(count,"times fuck you Rihan bhosdike");}
+
