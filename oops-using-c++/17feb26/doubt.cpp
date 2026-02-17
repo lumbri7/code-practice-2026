3fa24c25076f00709c5c58706b32b68c6f2396b7
lumbri7 <lumbrinitin@gmail.com>
2026-02-17 15:48:34 +0530

    opps doubt file

---
+++ oops-using-c++/17feb26/doubt.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class emp{
+
+public:
+    char work;
+    float attendance;
+
+private:
+    int salary;
+
+    void setsal(int salary){
+        this->salary = salary;
+    }
+
+    int getsal(){
+        return salary;
+    }
+
+    void display(){
+        cout << "Employee work: " << work << endl;
+        cout << "Attendance: " << attendance << endl;
+        cout << "Salary: " << salary << endl;
+    }
+};
+int main(){
+    emp e1;
+    e1.work= A ;
+    e1.attendance = 78.5;
+    e1.setsal(28000);
+    e1.display();
+
+    return 0;
+}
