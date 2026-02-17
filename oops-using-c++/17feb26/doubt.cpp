eb8435790845351ae0c2ea6fe07517226811662d
lumbri7 <lumbrinitin@gmail.com>
2026-02-17 16:50:48 +0530

    Files removed jo faltu thi

--- oops-using-c++/17feb26/doubt.cpp
+++ oops-using-c++/17feb26/doubt.cpp
@@ -10,6 +10,9 @@ public:
 private:
     int salary;
 
+public:
+
+
     void setsal(int salary){
         this->salary = salary;
     }
@@ -26,7 +29,7 @@ private:
 };
 int main(){
     emp e1;
-    e1.work= A ;
+    e1.work = 'A';
     e1.attendance = 78.5;
     e1.setsal(28000);
     e1.display();
